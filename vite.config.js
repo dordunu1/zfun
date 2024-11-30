5b72162f1d485f5806cf2f6d567c9f828479f616
dordunu1 <dordunuchristian51@gmail.com>
2024-11-30 09:01:40 +0000

    add recharts

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,27 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'recharts': 'recharts'
+    }
+  },
   build: {
+    commonjsOptions: {
+      include: [/recharts/, /node_modules/]
+    },
     rollupOptions: {
-      external: ['recharts'],
       output: {
-        globals: {
-          recharts: 'Recharts'
+        manualChunks: {
+          'recharts': ['recharts']
         }
       }
     }
   },
   optimizeDeps: {
     include: ['recharts']
+  },
+  server: {
+    host: true
   }
 });
