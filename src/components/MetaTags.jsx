e089d97a02ebb6f7e313f927f24745d40fe2132e
dordunu1 <dordunuchrstian51@gmail.com>
2025-02-23 14:54:37 +0000

    loading issues

--- src/components/MetaTags.jsx
+++ src/components/MetaTags.jsx
@@ -19,12 +19,16 @@ const MetaTags = () => {
       <meta property="og:url" content={meta.url} />
       <meta property="og:title" content={meta.title} />
       <meta property="og:description" content={meta.description} />
+      <meta property="og:image" content={meta.image} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
 
       {/* Twitter */}
-      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={meta.url} />
       <meta property="twitter:title" content={meta.title} />
       <meta property="twitter:description" content={meta.description} />
+      <meta property="twitter:image" content={meta.image} />
     </Helmet>
   );
 };
